c7d46d507293fc93082bc19b8311c3f100b1f127
Oscar Freij <otg020313@gmail.com>
2020-02-19 21:56:13 +0100

    Adding content to page.
    Adding content to the page and did some restructuring.

--- private_html/pages/home.php
+++ private_html/pages/home.php
@@ -20,7 +20,7 @@
         <div class="nav-bar-panel">
           <a id="home-button" data-active="false" data-show="home" id="home-button" class="nav-button" href="#home" onclick="changeView('home')"><p>Home</p></a>
           <a id="about-button" data-active="false" data-show="about" id="about-button" class="nav-button" href="#about" onclick="changeView('about')"><p>About Us</p></a>
-          <a id="service-button" data-active="false" data-show="service" id="service-button" class="nav-button" href="#service" onclick="changeView('service')"><p>Our Services</p></a>
+          <a id="mission-button" data-active="false" data-show="mission" id="mission-button" class="nav-button" href="#mission" onclick="changeView('mission')"><p>Our Mission</p></a>
           <a id="partners-button" data-active="false" data-show="partners" id="partners-button" class="nav-button" href="#partners" onclick="changeView('partners')"><p>Partners</p></a>
           <a id="join-button" data-active="false" data-show="join" id="join-button" class="nav-button" href="#join" onclick="changeView('join')"><p>Join The Ranks</p></a>
         </div>
@@ -28,38 +28,62 @@
           <div id="home-page" class="content-page" data-active="false" data-show="home">
             <h1>Hello and welcome!</h1>
             <div>
-              <p>Hello fellow citizen, you are about to experience an advertisement for the Rising Sun Corporation.</p>
-              <p>Ever dreamed of living the good exo-skeleton life? Consider joining the Rising Sun Corporation!</p>
-              <p>The main goal of the Rising Sun Corporation is to establish economic stability and peace in the new sector. Our focus lies in extracting and selling goods to any clients and persons of interest. We rely on extraction, mining and guarding jobs to keep the Universe clean.</p>
-              <p>However, we sell our armed forces to the highest bidder for a reasonable price.  Additionally, we have a designated research and development unit that creates new means of warfare, weaponry, and space travel to ensure safety and balance in our area of influence.  We are open to alliance and partnership requests at any point in time.</p>
-              <p>If you like to join a highly organized group make an impact in the galaxy, we have the job for you.  From armed patron to ore extractor, our Corporation gives you a chance to fulfill whatever career you are dreaming of.</p>
-              <h2>Consider joining The Rising Sun Corporation today to make the new frontier a better place, your cozy quarters are waiting for you!</h3>
+            <p>
+            This is the official website of the Rising Sun Corporation.
+            <br>
+            Fell free to look around and learn about us and our values!
+            </p>
+            <br>
+            <p>Remember: "Trade to Prosper"</p>
             </div>
           </div>
           <div id="about-page" class="content-page" data-active="false" data-show="about">
-            <h1>Title</h1>
+            <h1>About Us</h1>
             <div>
-              General Stats: Focus: Economic and Militaristic Prosperity  Allegiance: Neutral Colour Scheme: Yellow, Black, White
-              <br>
-              Content. Fill me with SHIT!
+            <p>
+            The Rising Sun Corporation [RSC] was founded on 12th June 2019 by Shepard and Flooxy.
+            <br>
+            <br>
+            Our the Staff hierarchy into a tier list, based off of a simplified Military ranking system, conjoined with a Corporation ranking tree within the faction.
+            <br>
+            Join the discord server to see more info about our ranks!
+            </p>
             </div>
           </div>
-          <div id="service-page" class="content-page" data-active="false" data-show="service">
-            <h1>Title</h1>
-              <div>
-                Content. Fill me with SHIT!
-              </div>
+          <div id="mission-page" class="content-page" data-active="false" data-show="mission">
+          <h1>Our Mission</h1>
+            <div>
+            <p>
+            The main goal of the Rising Sun Corporation is to establish economic stability and peace in the new sector.
+            <br>
+            Our focus lies in extracting and selling goods to any clients and persons of interest. RSC relies on extraction, mining and guarding jobs to keep the Universe clean.
+            <br>
+            However, we sell our armed forces to the highest bidder for a reasonable price.
+            <br>
+            The Rising Sun Corporation is a militaristic "PMC" (Private Military Company) with its main trade bringing in trade resources, mining and other cargo oriented workforces.
+            </p>
+            </div>
           </div>
           <div id="partners-page" class="content-page" data-active="false" data-show="partners">
-            <h1>Title</h1>
+            <h1>Our Partners</h1>
               <div>
-                Content. Fill me with SHIT!
+                <p>
+                Our current partners:
+                <ul>
+                <li><p><a href="https://wiki.starbasegame.com/index.php?search=Lodestar&title=Special%3ASearch%7C">Lodestar alliance</a> Since the 23rd of October 2019</p></li>
+                <li></li>
+                </ul>
+                </p>
               </div>
           </div>
           <div id="join-page" class="content-page" data-active="false" data-show="join">
-            <h1>Title</h1>
+            <h1>Join The Ranks</h1>
               <div>
-                Content. Fill me with SHIT!
+                <p>
+                Intrested in joining our ranks or to make business with us?
+                <br>
+                Join our discord and apply for a roll accordingly.
+                </p>
                 <br>
                 <br>
                 <p>Link to our Discord: <a href="https://discord.gg/cwBjB5n">https://discord.gg/cwBjB5n</a></p>
--- public_html/index.php
+++ public_html/index.php
@@ -3,19 +3,19 @@ $i = 0;
 try {
 switch ($i) {
     case "0":
-        include 'pages/home.php';
+        include '../private_html/pages/home.php';
         break;
     case "1":
-        include 'pages/maintenance.php';
+        include '../private_html/pages/maintenance.php';
         break;
     case "2":
-        include 'pages/update.php';
+        include '../private_html/pages/update.php';
         break;
 	case "3":
         echo("BAKA! BAKA! BAKA! BAKAAAAAAA!");
         break;
 	default:
-		include 'pages/home.php';
+		include '../private_html/pages/home.php';
 		break;
 }
 } catch (Exception $e) {
